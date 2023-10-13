b23b6743f941bfaf3e1b4f41e3445f6c81fbecad
Kevin Ladan <kevinladan@outlook.com>
2023-10-13 12:05:28 +0800

    Xcode 14.3.1 fix

--- Sources/NavigationBarStyles/IndicatorBarView.swift
+++ Sources/NavigationBarStyles/IndicatorBarView.swift
@@ -19,7 +19,7 @@ internal struct IndicatorBarView<Indicator, BG>: View where Indicator: View, BG:
                 background()
                 let totalItemWidth = (settings.width - (internalStyle.tabItemSpacing * CGFloat(dataStore.itemsCount - 1)))
                 let navBarItemWidth = totalItemWidth / CGFloat(dataStore.itemsCount)
-                if let navBarItemWidth, navBarItemWidth > 0, navBarItemWidth <= settings.width {
+                if navBarItemWidth > 0, navBarItemWidth <= settings.width {
                     let x = -settings.contentOffset / CGFloat(dataStore.itemsCount) + navBarItemWidth / 2
                     indicator()
                         .animation(.default)
